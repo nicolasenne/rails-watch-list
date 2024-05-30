166c9f5ee7c1da61304664a6f6425aeea124f42e
Nicolas Enne <40183143+nicolasenne@users.noreply.github.com>
2024-05-30 12:54:50 -0300

    migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,18 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_21_075255) do
+ActiveRecord::Schema[7.0].define(version: 2024_05_30_152243) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "bookmarks", force: :cascade do |t|
-    t.text "comment"
-    t.bigint "movie_id", null: false
-    t.bigint "list_id", null: false
+    t.string "comment"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["list_id"], name: "index_bookmarks_on_list_id"
-    t.index ["movie_id"], name: "index_bookmarks_on_movie_id"
   end
 
   create_table "lists", force: :cascade do |t|
