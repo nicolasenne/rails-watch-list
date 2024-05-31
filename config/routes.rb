f613deca3d1a5e3ae6c3b33f5b7f57c9d3781d0f
Nicolas Enne <40183143+nicolasenne@users.noreply.github.com>
2024-05-31 18:09:03 -0300

    movies

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  # root 'lists#index'
-  resources :lists, only: %i[index show new create destroy] do
+  root 'lists#index'
+  resources :lists, only: %i[show new create destroy] do
     resources :bookmarks, only: %i[index new create]
   end
   resources :bookmarks, only: :destroy
