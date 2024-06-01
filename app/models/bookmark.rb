00f3fc8630721ca3f070f46cf4fb7e8ced613777
Nicolas Enne <40183143+nicolasenne@users.noreply.github.com>
2024-05-31 21:47:51 -0300

    im tired

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -2,6 +2,6 @@ class Bookmark < ApplicationRecord
   belongs_to :movie
   belongs_to :list
 
-  validates :comment, length: { minimum: 6 }
+  validates :comment, length: { minimum: 6, maximum: 30 }
   validates :movie_id, uniqueness: { scope: :list_id, message: "Already exists" }
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,14 +8,14 @@
 
 Movie.create(
   title: "Wonder Woman 1984", 
-  overview: "Wonder Woman comes into conflict with the Soviet Union during the Cold War in the 1980s", 
+  overview: "Wonder Woman comes into conflict with the Soviet Union during the Cold War in the 1980s.", 
   poster_url: "https://image.tmdb.org/t/p/original/8UlWHLMpgZm9bx6QYh0NFoq67TZ.jpg", 
   rating: 6.9
 )
 
 Movie.create(
   title: "The Shawshank Redemption", 
-  overview: "Framed in the 1940s for double murder, upstanding banker Andy Dufresne begins a new life at the Shawshank prison", 
+  overview: "Framed in the 1940s for double murder, upstanding banker Andy Dufresne begins a new life at the Shawshank prison.", 
   poster_url: "https://image.tmdb.org/t/p/original/q6y0Go1tsGEsmtFryDOJo3dEmqu.jpg", 
   rating: 8.7
 )
